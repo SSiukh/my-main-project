e36f4bf7dde7a520cef76de99b85687b95d7fcd3
SSiukh <siukhsasha@gmail.com>
2024-07-13 21:36:59 +0300

    all

--- scriptlev5.js
+++ scriptlev5.js
@@ -1,4 +1,6 @@
 let taskComplete = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let checkType = [0, 0, 0];
+Tetris();
 
 const task1 = {
   question: "У якому році відбулося хрещення Київської Русі?",
@@ -333,6 +335,226 @@ function Level(taskNumb) {
         checkLineElements,
         task10.task
       );
+
       break;
   }
+  if (taskComplete[9] === 1) {
+    Recomplete();
+  }
+}
+
+function Recomplete() {
+  const main = document.getElementById("lev5-main-div");
+  const question = document.getElementById("lev5-question-div");
+  main.removeChild(question);
+
+  const completeBlocks = document.getElementsByClassName("lev5-complete-block");
+
+  setTimeout(() => {
+    for (let i = 0; i < 10; i++) {
+      completeBlocks[i].classList.remove("lev5-complete-animation");
+      completeBlocks[i].classList.remove("lev5-uncomplete-animation");
+      completeBlocks[i].classList.add("lev5-complete-block-disappear");
+    }
+    setTimeout(() => {
+      for (let i = 0; i < 10; i++) {
+        completeBlocks[i].classList.remove("lev5-complete-block-disappear");
+        completeBlocks[i].classList.add("lev5-complete-block-transparent");
+      }
+    }, 3000);
+  }, 2000);
+  setTimeout(() => {
+    main.style.height = "1800px";
+    TextTyping(0);
+    document.querySelector(".code-address").style.display = "flex";
+    document.querySelector(".code-address").style.position = "absolute";
+    document.querySelector(".code-address").style.top = "1400px";
+  }, 8000);
+}
+
+function Tetris() {
+  const main = document.getElementById("lev5-main-div");
+  let elements = [];
+  for (let i = 0; i < 18; i++) {
+    elements[i] = document.createElement("div");
+    elements[i].classList.add("tetris-elements");
+    main.appendChild(elements[i]);
+    elements[i].style.position = "absolute";
+  }
+  elements[0].style.top = "600px";
+  elements[0].style.left = "90px";
+  elements[1].style.top = "600px";
+  elements[1].style.left = "140px";
+  elements[2].style.top = "600px";
+  elements[2].style.left = "190px";
+  elements[3].style.top = "650px";
+  elements[3].style.left = "90px";
+
+  elements[4].style.top = "1100px";
+  elements[4].style.left = "190px";
+  elements[5].style.top = "1150px";
+  elements[5].style.left = "190px";
+
+  elements[6].style.top = "1500px";
+  elements[6].style.left = "140px";
+  elements[7].style.top = "1550px";
+  elements[7].style.left = "90px";
+  elements[8].style.top = "1550px";
+  elements[8].style.left = "140px";
+
+  elements[4].style.display = "none";
+  elements[5].style.display = "none";
+  elements[6].style.display = "none";
+  elements[7].style.display = "none";
+  elements[8].style.display = "none";
+
+  elements[9].style.top = "355px"; //змна на 22 + then -10
+  elements[9].style.right = "150px";
+  elements[10].style.top = "355px";
+  elements[10].style.right = "100px";
+  elements[11].style.top = "405px";
+  elements[11].style.right = "100px";
+  elements[12].style.top = "455px";
+  elements[12].style.right = "100px";
+
+  elements[13].style.top = "1050px";
+  elements[13].style.right = "200px";
+  elements[14].style.top = "1100px";
+  elements[14].style.right = "200px";
+  elements[15].style.top = "1100px";
+  elements[15].style.right = "150px";
+
+  elements[16].style.top = "1550px";
+  elements[16].style.right = "200px";
+  elements[17].style.top = "1550px";
+  elements[17].style.right = "150px";
+
+  elements[13].style.display = "none";
+  elements[14].style.display = "none";
+  elements[15].style.display = "none";
+  elements[16].style.display = "none";
+  elements[17].style.display = "none";
+}
+
+// приклда зміни сторінки при скролі
+
+const initialPos = [
+  600, 600, 600, 650, 1100, 1150, 1500, 1550, 1550, 355, 355, 405, 455, 1050,
+  1100, 1100, 1550, 1550,
+];
+
+let scrollPosition = 0;
+window.addEventListener("scroll", () => {
+  scrollPosition = window.scrollY;
+  console.log(scrollPosition);
+
+  const mainDiv = document.querySelector("#lev5-main-div");
+  const firstElements = document.getElementsByClassName("tetris-elements");
+
+  if (mainDiv.style.height === "1800px") {
+    firstElements[4].style.display = "block";
+    firstElements[5].style.display = "block";
+    firstElements[6].style.display = "block";
+    firstElements[7].style.display = "block";
+    firstElements[8].style.display = "block";
+    firstElements[13].style.display = "block";
+    firstElements[14].style.display = "block";
+    firstElements[15].style.display = "block";
+    firstElements[16].style.display = "block";
+    firstElements[17].style.display = "block";
+  }
+
+  if (scrollPosition > 100) {
+    mainDiv.classList.add("main-div-animation-five");
+    mainDiv.classList.remove("main-div-animation-five-back"); // Remove the back class if added
+  } else {
+    mainDiv.classList.remove("main-div-animation-five"); // Remove the forward class
+  }
+
+  //  таким чином зробити для всіх елементів, елементи що розташовуються після потрібно ініціювати пізніше або ж показати
+  for (let i = 0; i < 4; i++) {
+    firstElements[i].style.top = initialPos[i] + scrollPosition * 0.8 + "px";
+  }
+
+  for (let i = 9; i < 13; i++) {
+    firstElements[i].style.top = initialPos[i] + scrollPosition + "px";
+  }
+
+  if (scrollPosition >= 560) {
+    firstElements[4].style.top =
+      initialPos[2] + 50 + scrollPosition * 0.8 + "px";
+    firstElements[5].style.top =
+      initialPos[2] + 100 + scrollPosition * 0.8 + "px";
+    // firstElements[13].style.top = initialPos[9] + scrollPosition + "px";
+  } else {
+    // Залишаємо 5-й елемент на місці
+    firstElements[4].style.top = initialPos[4] + "px";
+    firstElements[5].style.top = initialPos[5] + "px";
+    firstElements[13].style.top = initialPos[13] + "px";
+  }
+
+  if (scrollPosition >= 695) {
+    firstElements[13].style.top = initialPos[9] + scrollPosition + "px";
+    firstElements[14].style.top = initialPos[9] + 50 + scrollPosition + "px";
+    firstElements[15].style.top = initialPos[9] + 50 + scrollPosition + "px";
+  } else {
+    firstElements[13].style.top = initialPos[13] + "px";
+    firstElements[14].style.top = initialPos[14] + "px";
+    firstElements[15].style.top = initialPos[15] + "px";
+  }
+
+  if (scrollPosition >= 1060) {
+    firstElements[6].style.top =
+      initialPos[2] + 50 + scrollPosition * 0.8 + "px";
+    firstElements[7].style.top =
+      initialPos[2] + 100 + scrollPosition * 0.8 + "px";
+    firstElements[8].style.top =
+      initialPos[2] + 100 + scrollPosition * 0.8 + "px";
+  } else {
+    // Залишаємо 5-й елемент на місці
+    firstElements[6].style.top = initialPos[6] + "px";
+    firstElements[7].style.top = initialPos[7] + "px";
+    firstElements[8].style.top = initialPos[8] + "px";
+  }
+
+  if (scrollPosition >= 1095) {
+    firstElements[16].style.top = initialPos[9] + 100 + scrollPosition + "px";
+    firstElements[17].style.top = initialPos[9] + 100 + scrollPosition + "px";
+  } else {
+    firstElements[16].style.top = initialPos[16] + "px";
+    firstElements[17].style.top = initialPos[17] + "px";
+  }
+});
+
+function TextTyping(numb) {
+  document.getElementById("lev5-main-div").style.justifyContent = "start";
+  document.getElementById("lev5-main-div").style.paddingTop = "300px";
+  let i = 0;
+  const speed = 150;
+  let text = [];
+  text[0] = "Був радий вітати вас на бета-версії нашого путівця!";
+  text[1] = "Колись це стане повноцінним проєктом, але наразі ";
+  text[2] = "це продукт для портфоліо. Гортайте донизу для зв'язку;)";
+  const elements = [
+    document.getElementById("typing-row-0"),
+    document.getElementById("typing-row-1"),
+    document.getElementById("typing-row-2"),
+  ];
+
+  function typeWriter() {
+    if (i < text[numb].length) {
+      elements[numb].textContent += text[numb].charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+      if (i === text[numb].length && numb < 2) {
+        elements[numb].style.borderRight = "none";
+        TextTyping(numb + 1);
+        if (numb === 2) {
+          TetrisContinue();
+        }
+      }
+    }
+  }
+
+  typeWriter();
 }
